4bfb25900abd24314f114dcc20edb6ffdb336872
pataz1k <alekseiloker1@gmail.com>
2024-03-21 16:41:01 +0500

    full migrate to TS

--- React/src/Helper/urlContext.js
+++
@@ -1,9 +0,0 @@
-const mainUrl = 'http://127.0.0.1:8000/'
-
-export const petsURL = mainUrl + 'pets_list'
-
-export const loginURL = mainUrl + 'login'
-
-export const signupURL = mainUrl + 'signup'
-
-export const userUrl = mainUrl + 'users'
